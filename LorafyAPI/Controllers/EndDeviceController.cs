326f537c383668d5855b85c7deae1d0f8783f92b
Stijn Stroeve <stijnstroeve@hotmail.com>
2022-12-02 12:14:28 +0100

    Added test entity and controller.

---
+++ LorafyAPI/Controllers/EndDeviceController.cs
@@ -0,0 +1,24 @@
+﻿using LorafyAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LorafyAPI.Controllers
+{
+    [ApiController]
+    [Route("api/end-device")]
+    public class EndDeviceController : ControllerBase
+    {
+        // In The end, this should return a Model instead of an Entity. But this is fine for testing purposes.
+        [HttpGet]
+        public EndDevice Get()
+        {
+            return new EndDevice
+            {
+               EUI = "A84041C1818350AD",
+               Address = "260B6BA6",
+               Name = "lht-gronau",
+               DateCreated = DateTime.Now,
+               DateUpdated = DateTime.Now,
+            };
+        }
+    }
+}
---
+++ LorafyAPI/Entities/EndDevice.cs
@@ -0,0 +1,11 @@
+﻿namespace LorafyAPI.Entities
+{
+    public class EndDevice
+    {
+        public string EUI { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+    }
+}
