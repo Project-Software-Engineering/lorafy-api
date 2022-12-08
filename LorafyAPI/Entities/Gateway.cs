a29a441f12c550d0687aa0705bce0a09f5335eb6
Stijn Stroeve <stijnstroeve@hotmail.com>
2022-12-08 10:21:44 +0100

    Added initial entities.

---
+++ LorafyAPI/Entities/Gateway.cs
@@ -0,0 +1,21 @@
+﻿
+namespace LorafyAPI.Entities
+{
+    public class GatewayLocation
+    {
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public float Altitude { get; set; }
+    }
+
+    public class Gateway
+    {
+        public string EUI { get; set; }
+        public string Name { get; set; }
+        public int RSSI { get; set; }
+        public float SNR { get; set; }
+        public GatewayLocation Location { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+    }
+}
---
+++ LorafyAPI/Entities/UplinkMessage.cs
@@ -0,0 +1,35 @@
+﻿
+namespace LorafyAPI.Entities
+{
+    public class UplinkMessagePayload
+    {
+        public float Battery { get; set; }
+        public float BatteryVoltage { get; set; }
+        public float TemperatureInside { get; set; }
+        public float TemperatureOutside { get; set; }
+        public float Humidity { get; set; }
+        public float Light { get; set; }
+        public float Pressure { get; set; }
+    }
+
+    public class UplinkMessageDataRate
+    {
+        public int Bandwidth { get; set; }
+        public int SpreadingFactor { get; set; }
+        public string CodingRate { get; set; }
+    }
+
+    public class UplinkMessage
+    {
+        public int Id { get; set; }
+        public string EndDeviceEUI { get; set; }
+        public EndDevice EndDevice { get; set; }
+        public string GatewayEUI { get; set; }
+        public Gateway Gateway { get; set; }
+        public UplinkMessagePayload Payload { get; set; }
+        public UplinkMessageDataRate DataRate { get; set; }
+        public DateTime DateReceived { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+    }
+}
