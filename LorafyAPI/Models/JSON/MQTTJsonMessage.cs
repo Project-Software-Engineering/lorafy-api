6190981a57da2daa633a5af0eac68ab796ef6303
Stijn Stroeve <stijnstroeve@hotmail.com>
2023-01-12 12:16:51 +0100

    Fixed LoRaPy service.

--- LorafyAPI/Models/JSON/MQTTJsonMessage.cs
+++ LorafyAPI/Models/JSON/MQTTJsonMessage.cs
@@ -5,7 +5,7 @@
         public MQTTJsonEndDeviceIds end_device_ids { get; set; }
         public List<string> correlation_ids { get; set; }
         public string received_at { get; set; }
-        public MQTTJsonUplinkMessage uplink_message { get; set; }
+        public MQTTJsonUplinkMessage? uplink_message { get; set; }
     }
 }
 
